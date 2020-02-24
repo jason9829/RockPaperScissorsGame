11e07c970e740609619bbc707baa810018382999
jaosn9829 <jason9829@live.com>
2020-02-24 14:16:07 +0800

    Simple version of the game.
    Prototype for the GUI version of the game, some features and improvement will be updated soon.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -4,14 +4,23 @@
 from tkinter import *
 from random import randint
 import ctypes  # An included library with Python install.
-
+import sys
 
 #  Pop up GUI
 popup = Tk()    # For user select game rounds
 main = Tk()     # Main Pop up
 
+#  Game Status Flag/ To start or end the game
+STOP_GAME_FLAG = 999
+
+# Gameplay status
+userWinCount = 0
+tieCount = 0
+computerWinCount = 0
+gameRound = 1
+
 #  Number of game rounds
-globalGameRound = 0
+globalGameRound = STOP_GAME_FLAG
 
 #  Player play options
 globalUserPlayOption = "None"
@@ -85,83 +94,184 @@ def gameManager():
 
         gameRound -= 1
 
-
-if __name__ == "__main__":
     # Commented out for unittests else hang instantiating tests...
     # Uncomment to test the game here
-    #gameManager()
-    #main()
-#  ref: https://www.youtube.com/watch?v=ELkaEpN29PU
+    # gameManager()
 
-#  -------------------GUI-----------------------------
-#   Create windows object
 
-    def userSelectRock():
-        global globalUserPlayOption
-        globalUserPlayOption = "Rock"
-        print("You selected Rock")
+#  -----------------For GUI (Not for Test)------------------------
+#  Desc: Determine the winner when the game is not tied
+#        (Assume that the options are valid) - GUI version
+#  Param: User and computer play options
+#  Retval: Return result of the game in string
+def gameNotTiedHandlerGUI(userOption, computerOption):
+    if userOption.lower() == "rock" and computerOption.lower() == "scissors":
+        global userWinCount
+        userWinCount += 1
+        print(userWinCount)
+        return "You win! Rock beats Scissors."
+    elif userOption.lower() == "rock" and computerOption.lower() == "paper":
+        global computerWinCount
+        computerWinCount += 1
+        print(computerWinCount)
+        return "You lose! Paper beats Rock."
+    elif userOption.lower() == "paper" and computerOption.lower() == "rock":
+        userWinCount += 1
+        return "You win! Paper beats Rock."
+    elif userOption.lower() == "paper" and computerOption.lower() == "scissors":
+        computerWinCount += 1
+        return "You lose! Scissors beats Paper."
+    elif userOption.lower() == "scissors" and computerOption.lower() == "paper":
+        userWinCount += 1
+        return "You win! Scissors beats Paper."
+    elif userOption.lower() == "scissors" and computerOption.lower() == "rock":
+        computerWinCount += 1
+        return "You lose! Rock beats Scissors."
 
-    def userSelectPaper():
-        global globalUserPlayOption
-        globalUserPlayOption = "Paper"
-        print("You selected Paper")
 
-    def userSelectScissors():
-        global globalUserPlayOption
-        globalUserPlayOption = "Scissors"
-        print("You selected Scissors")
+# Show the score between user, tie and computer
+# Ref: https://stackoverflow.com/questions/31707206/tkinter-label-counter-variable
+def updateScoreBoard():
+    userWin_Label = Label(main, text="User Win: " + str(userWinCount))
+    userWin_Label.place(x=110, y=150)
 
-    def userInputRound(arg):
-        print(arg)
+    gameTie_Label = Label(main, text="Tie: " + str(tieCount))
+    gameTie_Label.place(x=110, y=210)
 
-    def userSelectOneRound():
-        global globalGameRound
-        globalGameRound = 1
-        popup.destroy()
-        print(globalGameRound)
+    ComputerWin_Label = Label(main, text="Computer Win: " + str(computerWinCount))
+    ComputerWin_Label.place(x=110, y=270)
 
-    def userSelectThreeRound():
-        global globalGameRound
-        globalGameRound = 3
-        popup.destroy()
-        print(globalGameRound)
 
-    def userSelectTenRound():
-        global globalGameRound
-        globalGameRound = 10
-        popup.destroy()
-        print(globalGameRound)
+#  Update the game round and display on main windows
+def updateGameRound():
+    gameRound_Label = Label(main, text="Game round: " + str(gameRound))
+    gameRound_Label.config(font=("Courier", 10))
+    gameRound_Label.place(x=370, y=20)
+
+
+def userSelectRock():
+    global globalUserPlayOption
+    globalUserPlayOption = "Rock"
+    print("You selected Rock")
+    main.quit()
+    # quit() will close the window but not mainloop()
+    # destroy() will close the windows and mainloop()
+
+
+def userSelectPaper():
+    global globalUserPlayOption
+    globalUserPlayOption = "Paper"
+    print("You selected Paper")
+    main.quit()
+
+
+def userSelectScissors():
+    global globalUserPlayOption
+    globalUserPlayOption = "Scissors"
+    print("You selected Scissors")
+    main.quit()
+
+
+def userInputRound(arg):
+    print(arg)
+
+
+def userSelectOneRound():
+    global globalGameRound
+    globalGameRound = 1
+    popup.destroy()
+    print(globalGameRound)
+
+
+def userSelectThreeRound():
+    global globalGameRound
+    globalGameRound = 3
+    popup.destroy()
+    print(globalGameRound)
+
+
+def userSelectTenRound():
+    global globalGameRound
+    globalGameRound = 10
+    popup.destroy()
+    print(globalGameRound)
+
+
+def userSelectExit():
+    sys.exit()
 
 
 #  Create an popup message box for user to select game round
+#  Ref: https://www.youtube.com/watch?v=4McKSuuUQ-0
 def popupMsgSelectGameRound(msg):
 
     popup.wm_title("Game Menu")
     label = Label(popup, text=msg)
     label.pack(side="top", fill="x", pady=10)
-    B1 = Button(popup, text="1", command=userSelectOneRound)
+    B1 = Button(popup, text="1", width=10, height=5, command=userSelectOneRound)
     B1.pack()
-    B2 = Button(popup, text="3", command=userSelectThreeRound)
+    B2 = Button(popup, text="3", width=10, height=5, command=userSelectThreeRound)
     B2.pack()
-    B3 = Button(popup, text="10", command=userSelectTenRound)
+    B3 = Button(popup, text="10", width=10, height=5, command=userSelectTenRound)
     B3.pack()
+    B4 = Button(popup, text="Exit", width=10, height=5, command=userSelectExit)
+    B4.pack()
     popup.deiconify()
 
 
+# GUI menu to play the game
 def popupMain():
+    main.resizable(False, False)
     main.title('Rock, Paper and Scissors Game, by Jason')
     main.geometry('500x350')
 
     #  Create the play option buttons
-    main.ButtonRock = Button(main, text="Rock", command=userSelectRock).place(x=2, y=2)
-    main.ButtonPaper = Button(main, text="Paper", command=userSelectPaper).place(x=50, y=50)
-    main.ButtonScissors = Button(main, text="Scissors", command=userSelectScissors).place(x=100, y=100)
+    #  Ref: https://stackoverflow.com/questions/46284901/how-do-i-resize-buttons-in-pixels-tkinter/
+    #  Reference to resize the button
+    main.ButtonRock = Button(main, text="Rock", command=userSelectRock).place(x=0, y=0, width=100, height=116)
+    main.ButtonPaper = Button(main, text="Paper", command=userSelectPaper).place(x=0, y=116, width=100, height=116)
+    main.ButtonScissors = Button(main, text="Scissors", command=userSelectScissors).place(x=0, y=232, width=100,
+                                                                                          height=116)
 
-    main.mainloop()
+
+# GUI message box just to display the message
+# with an OK button
+def popupMsg(msg):
+    ctypes.windll.user32.MessageBoxW(0, msg, "Message", 16)
 
 
-popupMsgSelectGameRound("Please select the rounds to be played.")
-popupMain()
+#  Desc: Main function to call functions to perform
+#        the game - GUI version
+#  Param: None
+#  Retval: None
+def gameManagerGUI():
+    popupMsgSelectGameRound("Please select the rounds to be played or exit.")
+    global globalGameRound
+    global globalUserPlayOption
+    global gameRound
+
+    while globalGameRound != 0:
+        updateGameRound()
+        updateScoreBoard()
+        popupMain()
+        userPlayOption = globalUserPlayOption
+        computerPlayOption = playOptions[randint(0, 2)]
+        if isTheGameTied(userPlayOption, computerPlayOption):
+            global tieCount
+            tieCount += 1
+            updateScoreBoard()
+            popupMsg("Tie!")
+        else:
+            if globalGameRound != STOP_GAME_FLAG:
+                popupMsg(gameNotTiedHandlerGUI(userPlayOption, computerPlayOption))
+                updateScoreBoard()
+        globalGameRound -= 1
+        gameRound += 1
+        main.mainloop()
+    gameRound = 1   # Reset the value
+
+
+gameManagerGUI()
 
 
 
